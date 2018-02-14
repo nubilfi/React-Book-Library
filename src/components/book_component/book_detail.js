0d8cff67e3a60a866894e94c66bf638d0cc8cef8
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-14 11:03:49 +0700

    Use fetch instead of axios & set book state to be an array

--- src/components/book_component/book_detail.js
+++ src/components/book_component/book_detail.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import { Redirect, Link } from 'react-router-dom';
-import axios from 'axios';
-import _ from 'lodash';
+import moment from 'moment';
 
 import BookForm from './book_form.js';
 
-const ROOT_URL      = 'http://localhost:3001/api/v1';
+const ROOT_URL = 'http://localhost:3001/api/v1';
 
 class BookDetail extends Component {
 	constructor(props) {
@@ -14,31 +13,31 @@ class BookDetail extends Component {
 		const apiKey = localStorage.getItem('apiKey') || null;
 		this.state = {
 			authorization: apiKey,
-			book: {},
+			book: [],
 			isUpdate: false,
 			fireRedirect: false
 		}
 		this.renderDetail = this.renderDetail.bind(this);
-		this.onUpdate     = this.onUpdate.bind(this);
-		this.onDelete     = this.onDelete.bind(this);
+		this.onUpdate = this.onUpdate.bind(this);
+		this.onDelete = this.onDelete.bind(this);
 		this.handleChange = this.handleChange.bind(this);
 	}
 
 	componentDidMount() {
-		let book          = null;
-		const { id }        = this.props.match.params;
+		let book;
+		const { id } = this.props.match.params;
 		const Authorization = this.state.authorization;
 
-		axios
-			.get(`${ROOT_URL}/books/${id}`, { headers: { Authorization }})
-			.then((res) => {
-				book = res.data.results;
+		fetch(`${ROOT_URL}/books/${id}`, { headers: { Authorization }})
+			.then(res => res.json() )
+			.then((data) => {
+				book = data.results;
 				this.setState({ book });
 			});
 	}
 
 	handleChange(field, value) {
-		const state       = this.state.book;
+		const state = this.state.book;
 		
 		state[field] = value;
 		this.setState({state});
@@ -51,25 +50,22 @@ class BookDetail extends Component {
         <div className="well-block">
         <Link to="/books">Back</Link><br/><br/>
           <div className="well-title">
-              <h2>{book.fullname}</h2>
+              <h2>{book.title}</h2>
           </div>
           <div className="feature-block">
             <div className="feature feature-blurb-text">
-              <h4 className="feature-title">Get in touch</h4>
+              <h4 className="feature-title">Descriptions</h4>
               <div className="feature-content">
-                  <p>Want to send a feedback? Send an email to <strong>{book.email}</strong></p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio non reprehenderit est enim corrupti? Similique laboriosam, alias repellendus, vero veritatis doloremque ipsam amet, quam libero itaque aperiam eligendi incidunt ullam.</p>
               </div>
             </div>
             <div className="feature feature-blurb-text">
-              <h4 className="feature-title">Experienced</h4>
+              <h4 className="feature-title">Details</h4>
               <div className="feature-content">
-                  <p>Aliquam sit amet mi eu libero fermentum bibendum pulvinar a turpis. Vestibulum quis feugiat risus. </p>
-              </div>
-            </div>
-            <div className="feature feature-blurb-text">
-              <h4 className="feature-title">Published books</h4>
-              <div className="feature-content">
-                  <p>Praesent eu sollicitudin nunc. Cras malesuada vel nisi consequat pretium. Integer auctor elementum nulla suscipit in.</p>
+                  <p>Author: {book.author.fullname}</p>
+                  <p>Category: {book.category.category_name}</p>
+                  <p>Published: {moment(book.published).format('DD/MM/YYYY')}</p>
+                  <p>Page: {book.pages}</p>
               </div>
             </div>
           </div><br/>
@@ -88,28 +84,30 @@ class BookDetail extends Component {
 	}
 
 	onDelete() {
-		const { id }        = this.props.match.params;
+		const { id } = this.props.match.params;
 		const Authorization = this.state.authorization;
 
-		axios
-			.delete(`${ROOT_URL}/books/${id}`, { headers: { Authorization }})
-			.then((res) => {
-				this.setState({ fireRedirect: !this.state.fireRedirect });
-			});
+		fetch(`${ROOT_URL}/books/${id}`, {
+			headers: { Authorization },
+			method: 'DELETE'
+		})
+		.then((res) => {
+			this.setState({ fireRedirect: !this.state.fireRedirect });
+		});
 	}
 
 	render() {
 		const { book, isUpdate, fireRedirect } = this.state;
 
-		let theBook = null;
-		if (_.isEmpty(book)) {
+		let theBook;
+		if (book.length === 0) {
 			theBook = 'Loading...';
 		} else if(isUpdate) {
 			// set book and methods as props to the BookForm
 			theBook = <BookForm 
-										book={book}
-										onUpdate={this.onUpdate} 
-										onFieldChange={this.handleChange} />;
+									book={book}
+									onUpdate={this.onUpdate} 
+									onFieldChange={this.handleChange} />;
 		} else {
 			theBook = this.renderDetail(book);
 		}
