7f2c0a998d895dcb0600908e4bbae474038548b7
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-14 10:13:17 +0700

    Use fetch instead of axios & set author state to be an array

--- src/components/author_component/author_detail.js
+++ src/components/author_component/author_detail.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { Redirect, Link } from 'react-router-dom';
-import axios from 'axios';
-import _ from 'lodash';
 
 import AuthorForm from './author_form.js';
 
-const ROOT_URL      = 'http://localhost:3001/api/v1';
+const ROOT_URL = 'http://localhost:3001/api/v1';
 
 class AuthorDetail extends Component {
 	constructor(props) {
@@ -14,31 +12,31 @@ class AuthorDetail extends Component {
 		const apiKey = localStorage.getItem('apiKey') || null;
 		this.state = {
 			authorization: apiKey,
-			author: {},
+			author: [],
 			isUpdate: false,
 			fireRedirect: false
 		}
 		this.renderDetail = this.renderDetail.bind(this);
-		this.onUpdate     = this.onUpdate.bind(this);
-		this.onDelete     = this.onDelete.bind(this);
+		this.onUpdate = this.onUpdate.bind(this);
+		this.onDelete = this.onDelete.bind(this);
 		this.handleChange = this.handleChange.bind(this);
 	}
 
 	componentDidMount() {
-		let author          = null;
-		const { id }        = this.props.match.params;
+		let author;
+		const { id } = this.props.match.params;
 		const Authorization = this.state.authorization;
 
-		axios
-			.get(`${ROOT_URL}/authors/${id}`, { headers: { Authorization }})
-			.then((res) => {
-				author = res.data.results;
+		fetch(`${ROOT_URL}/authors/${id}`, { headers: { Authorization }})
+			.then(res => res.json() )
+			.then((data) => {
+				author = data.results;
 				this.setState({ author });
 			});
 	}
 
 	handleChange(field, value) {
-		const state       = this.state.author;
+		const state = this.state.author;
 		
 		state[field] = value;
 		this.setState({state});
@@ -61,15 +59,9 @@ class AuthorDetail extends Component {
               </div>
             </div>
             <div className="feature feature-blurb-text">
-              <h4 className="feature-title">Experienced</h4>
-              <div className="feature-content">
-                  <p>Aliquam sit amet mi eu libero fermentum bibendum pulvinar a turpis. Vestibulum quis feugiat risus. </p>
-              </div>
-            </div>
-            <div className="feature feature-blurb-text">
               <h4 className="feature-title">Published books</h4>
               <div className="feature-content">
-                  <p>Praesent eu sollicitudin nunc. Cras malesuada vel nisi consequat pretium. Integer auctor elementum nulla suscipit in.</p>
+                  <p>Check all published books by this author here:</p>
               </div>
             </div>
           </div><br/>
@@ -88,21 +80,23 @@ class AuthorDetail extends Component {
 	}
 
 	onDelete() {
-		const { id }        = this.props.match.params;
+		const { id } = this.props.match.params;
 		const Authorization = this.state.authorization;
 
-		axios
-			.delete(`${ROOT_URL}/authors/${id}`, { headers: { Authorization }})
-			.then((res) => {
-				this.setState({ fireRedirect: !this.state.fireRedirect });
-			});
+		fetch(`${ROOT_URL}/authors/${id}`, { 
+			headers: { Authorization },
+			method: 'DELETE'
+		})
+		.then((res) => {
+			this.setState({ fireRedirect: !this.state.fireRedirect });
+		});
 	}
 
 	render() {
 		const { author, isUpdate, fireRedirect } = this.state;
 
 		let theAuthor = null;
-		if (_.isEmpty(author)) {
+		if (author.length === 0) {
 			theAuthor = 'Loading...';
 		} else if(isUpdate) {
 			// set author and methods as props to the AuthorForm
